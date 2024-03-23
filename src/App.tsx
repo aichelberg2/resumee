d46c6682c72b31b0843dbdcc75f0c8bccc0c649f
Chris :) <102354839+aichelberg2@users.noreply.github.com>
2024-03-23 07:09:54 +0000

    Adjusted to codesandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,47 @@
-import './App.css';
-import SplineRender from './components/SplineRender';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import React, { useRef } from 'react';
+import "./App.css";
+//import SplineRender from './components/SplineRender';
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import React, { useRef } from "react";
 
-export type sidebarPageNameType = 'Welcome' | 'Gantry System' | 'Product Catalog' | 'Welding Guns' | 'Digital Learning' | 'Home Automation';
+export type sidebarPageNameType =
+  | "Welcome"
+  | "Gantry System"
+  | "Product Catalog"
+  | "Welding Guns"
+  | "Digital Learning"
+  | "Home Automation";
 
-export const sidebarPageIndexMap: { [ key in number ]: sidebarPageNameType } = {
-  0: 'Welcome',
-  1: 'Gantry System',
-  2: 'Product Catalog',
-  3: 'Welding Guns',
-  4: 'Digital Learning',
-  5: 'Home Automation',
+export const sidebarPageIndexMap: { [key in number]: sidebarPageNameType } = {
+  0: "Welcome",
+  1: "Gantry System",
+  2: "Product Catalog",
+  3: "Welding Guns",
+  4: "Digital Learning",
+  5: "Home Automation",
 };
 
 function App() {
-  const [ isSidebarOpen, setIsSidebarOpen ] = React.useState<boolean>(false);
-  const [ sidebarPageName, setSidebarPageName ] = React.useState<sidebarPageNameType>('Home Automation');
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState<boolean>(false);
+  const [sidebarPageName, setSidebarPageName] =
+    React.useState<sidebarPageNameType>("Home Automation");
   const headerRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className="resumee">
       {/* <SplineRender setSidebarPage={setSidebarPage} /> */}
-      <Header sidebarPageName={sidebarPageName} ref={headerRef} isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} setSidebarPageName={setSidebarPageName} />
-      <Sidebar sidebarPageName={sidebarPageName} isSidebarOpen={isSidebarOpen} headerRef={headerRef} />
+      <Header
+        sidebarPageName={sidebarPageName}
+        ref={headerRef}
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+        setSidebarPageName={setSidebarPageName}
+      />
+      <Sidebar
+        sidebarPageName={sidebarPageName}
+        isSidebarOpen={isSidebarOpen}
+        headerRef={headerRef}
+      />
     </div>
   );
 }
---
+++ src/descriptions/HomeAutomation.tsx
@@ -0,0 +1,38 @@
+import Dashboard from "../components/demo/Dashboard";
+
+const Welcome = () => {
+  return (
+    <div className="home-automation">
+      <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Welcome;
